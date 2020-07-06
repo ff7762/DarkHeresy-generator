897fe5c433a48b33e2cb0bad833127b54ce0f08c
ff7762 <67229067+ff7762@users.noreply.github.com>
2020-07-06 16:03:39 +0100

    Added traits, skiils and talents onto world objects. Created "homeworldtraits" function

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const worlds = [
-              ['Feral', 20, 20, 25, 25, 20, 20, 20, 15, 15, '9+d5', 0, 0, 0, 0],
-              ['Hive', 20, 20, 20, 15, 20, 20, 20, 20, 25, '8+d5', 0, 0, 0, 0],
-              ['Imperial', 20, 20, 20, 20, 20, 20, 20, 20, 20, '8+d5', 0, 0, 0, 0],
-              ['Void', 20, 20, 15, 20, 20, 20, 20, 25, 20, '6+d5', 0, 0, 0, 0],
-              ['Forge', 15, 20, 20, 20, 20, 25, 20, 20, 20, '7+d5', 0, 0, 0, 0]
+              ['Feral', 20, 20, 25, 25, 20, 20, 20, 15, 15, '9+d5', 0, 0, 0, 0, "Iron Stomach, Primitive, Rite of Passage, Wilderness Savvy", "Speak Language (Tribal Dialect) (Int); Navigation (Surface) (Int) (Wild. Sav.), Survival (Int) (Wild. Sav.), and Tracking (Int) (Wild. Sav.) as Basic Skills", "None"],
+              ['Hive', 20, 20, 20, 15, 20, 20, 20, 20, 25, '8+d5', 0, 0, 0, 0, "Accustomed to Crowds, Caves of Steel, Hivebound, Wary", "Speak Language (Hive Dialect) (Int); Tech-Use (Int) (Caves of Steel) as Basic Skill", "None"],
+              ['Imperial', 20, 20, 20, 20, 20, 20, 20, 20, 20, '8+d5', 0, 0, 0, 0, "Blessed Ignorance, Hagiography, Litugical Familiarity, Superior Origins", "Common Lore (Imperial Creed) (Int) (Hagi.), Common Lore (Imperium) (Int) (Hagi.), Common Lore (War) (Int) (Hagi.), Literacy (Int) (Lit. Fam.), Speak Language (High Gothic) (Int) (Lit. Fam.) as Basic Skills", "None"],
+              ['Void', 20, 20, 15, 20, 20, 20, 20, 25, 20, '6+d5', 0, 0, 0, 0, "Charmed, Ill-Omened, Shipwise, Void Accustomed", "Speak Language (Ship Dialect) (Int); Navigation (Stellar) (Int) (Shipwise), Pilot (Starcraft) (Ag) (Shipwise) as Basic Skills", "None"],
+              ['Forge', 15, 20, 20, 20, 20, 25, 20, 20, 20, '7+d5', 0, 0, 0, 0, "Fit for Purpose, Stranger to the Cult, Credo Omnissiah", "Common Lore (Tech) (Int) and Common Lore (Machine Cult) (Int) as Basic Skills", "Technical Knock (Credo Omnissiah)"]
              ];
 
 function isItemInArray(array, item) {
@@ -19,11 +19,13 @@ function isItemInArray(array, item) {
 
 document.getElementById("randomworld").addEventListener("click", function world_random() {
     var input = random();
-    base(input)
+    homeworldtraits(input);
+    base(input);
 });
 
 function random() {
     var world = worlds[Math.floor(Math.random() * worlds.length)];
+    console.log(world[15])
     return world;
 }
 
@@ -31,7 +33,7 @@ function base(world) {
     var world = worlds[Math.floor(Math.random() * worlds.length)];
     var table = document.getElementById("mytable");
     document.getElementById("world-dropdown").innerHTML='<option value="1">' + world[0] + '</option>';
-    populate_world_dropdown(worlds)
+    populate_world_dropdown(worlds);
     for (var i = 1, row; row = table.rows[i]; i++) {
         var cell = 0;
         var replace = world[i];
@@ -43,7 +45,14 @@ function base(world) {
             }
         }
     }
-    total()
+    total();
+}
+
+
+function homeworldtraits(world) {
+    document.getElementById("traits").innerHTML = "Homeworld Traits: "+ world[15];
+    document.getElementById("skills").innerHTML = "Homeworld Skills: "+ world[16];
+    document.getElementById("talents").innerHTML = "Homeworld Talents: "+ world[17];
 }
 
 function getworld_dropdown() {
@@ -56,6 +65,7 @@ document.getElementById("world-dropdown").addEventListener("change", function wo
     var input = getworld_dropdown();
     var world_object = isItemInArray(worlds, input);
     base(world_object);
+    homeworldtraits(world_object);
 });
 
 function populate_world_dropdown(worlds) {
