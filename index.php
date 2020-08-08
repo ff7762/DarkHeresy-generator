249ac4f0917e1340a4d9b966d457fad09b085c7e
ff7762 <67229067+ff7762@users.noreply.github.com>
2020-08-08 15:44:35 +0100

    Added style within $html variable to allow css for pdf HTML

--- index.php
+++ index.php
@@ -9,7 +9,20 @@ if (isset($_POST['attribute1'])) {
     $pdf->setPrintHeader(false);
     $pdf->setPrintFooter(false);
     $pdf->AddPage();
+    $html =
+    "
+    <style>
+    div {
+    color: #fcbfff;
+    }
+    </style>
+
+    <div class=background>
+        <h1 style=color:#ff11ff>XHTML Form Example</h1>
+    </div>
+    "
+    ;
+    $pdf->writeHTML($html, true, 0, true, 0);
     $pdf->cell(190,10,"WS: $var1",1,1,'C');
     $pdf->Output();
 }
-//add contents
