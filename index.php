d5293dfdd2908e64998f98c140bf370a1f4169b0
ff7762 <67229067+ff7762@users.noreply.github.com>
2020-08-08 16:50:33 +0100

    Added new Image

--- index.php
+++ index.php
@@ -14,20 +14,21 @@ if (isset($_POST['attribute1'])) {
     $height = $pdf->getPageHeight();
     $pdf->SetAutoPageBreak(false, 0);
     $pdf->Image('assets/background.jpg', 0, 0, $width, $height, 'JPG', '', '', false, 300, '', false, false, 0, false, false, true);
+    $pdf->Image('assets/logo.png', 10, 10, 22, 40, 'PNG', '', '', false, 300, '', false, false, 0, false, false, true);
     $html =
     "
     <style>
     div {
-    background-color: #00bfff;
+        height: 200px;
     }
     </style>
 
-    <div class=background>
-        <h1 style=color:#ff11ff>XHTML Form Example</h1>
-    </div>
+    <h1 style=color:#ff11ff></h1>
+    <div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div>
+    <hr>
     "
     ;
     $pdf->writeHTML($html, true, 0, true, 0);
-    $pdf->cell(190,10,"WS: $var1",1,1,'C');
+    /*$pdf->cell(190,10,"WS: $var1",1,1,'C');*/
     $pdf->Output();
 }
